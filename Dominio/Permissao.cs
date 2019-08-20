a92802cbe34e7b4fbe943a8663dfb8980f5cc239
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-20 19:30:57 -0400

    Criado o Dominio

---
+++ Dominio/Permissao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcModelBinding.Dominio
+{
+    public enum Permissao
+    {
+        Admin,
+        Usuario,
+        Convidado
+    }
+}
