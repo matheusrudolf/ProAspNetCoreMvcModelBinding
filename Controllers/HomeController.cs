9dbef5e69c11f958d045a4a737fecdd3bb2ef149
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-20 19:55:35 -0400

    Criado o HomeController na pasta Controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProAspNetCoreMvcModelBinding.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+        public HomeController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult Index(int id)
+        {
+            return View("Index", repository[id]);
+        }
+    }
+}
