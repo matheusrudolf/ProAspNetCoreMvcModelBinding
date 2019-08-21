d7126f506be7893473f0005bceb21061e94637b6
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-20 21:10:33 -0400

    Criado a classe EnderecoResumido em Models

---
+++ Models/EnderecoResumido.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcModelBinding.Models
+{
+    public class EnderecoResumido
+    {
+        public string Cidade { get; set; }
+        public string Pais { get; set; }
+    }
+}
