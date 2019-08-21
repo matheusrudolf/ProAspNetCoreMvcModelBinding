515a00b31be975696b4b2c541a5c1fb19e2c77a0
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-20 20:53:46 -0400

    Criado Endereço em Models

---
+++ Models/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcModelBinding.Models
+{
+    public class Endereco
+    {
+        public string Rua { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string CEP { get; set; }
+        public string Pais { get; set; }
+    }
+}
