28ff927892a06afd54332fb75a10bab2bc2b8a9c
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-20 21:36:55 -0400

    Alteração feita em EnderecoResumido em Models

--- Models/EnderecoResumido.cs
+++ Models/EnderecoResumido.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProAspNetCoreMvcModelBinding.Models
 {
+    [Bind(nameof(Cidade))]
     public class EnderecoResumido
     {
         public string Cidade { get; set; }
